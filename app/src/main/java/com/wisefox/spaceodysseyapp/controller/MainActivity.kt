e8c2471398ae13a4e6eb051f6cce254f2675d652
Maxime Traverse <maxime.traverse@orange.fr>
2021-01-31 20:13:13 +0100

    find & init text views

--- app/src/main/java/com/wisefox/spaceodysseyapp/controller/MainActivity.kt
+++ app/src/main/java/com/wisefox/spaceodysseyapp/controller/MainActivity.kt
@@ -4,17 +4,36 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import com.wisefox.spaceodysseyapp.R
 import com.wisefox.spaceodysseyapp.controller.quiz.QuizActivity
 
+
 class MainActivity : AppCompatActivity() {
+
+    // Graphics
+    private lateinit var tvTitle: TextView
+    private lateinit var tvSubTitle: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewsAndInitContent()
     }
+
 
     fun onClickQuizActivity(view: View) {
         val intentQuizActivity = Intent(this, QuizActivity::class.java)
         startActivity(intentQuizActivity)
+    }
+
+    private fun findViewsAndInitContent() {
+        //find views
+        tvTitle = findViewById(R.id.tv_title)
+        tvSubTitle = findViewById(R.id.tv_sub_title)
+
+        //init Content
+        tvTitle.text = getString(R.string.welcome)
+        tvSubTitle.text = getString(R.string.whatToDo)
     }
 }
--- app/src/main/java/com/wisefox/spaceodysseyapp/controller/quiz/QuizActivity.kt
+++ app/src/main/java/com/wisefox/spaceodysseyapp/controller/quiz/QuizActivity.kt
@@ -2,11 +2,29 @@ package com.wisefox.spaceodysseyapp.controller.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.wisefox.spaceodysseyapp.R
 
 class QuizActivity : AppCompatActivity() {
+
+    // Graphics
+    private lateinit var tvTitle: TextView
+    private lateinit var tvSubTitle: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+        findViewsAndInitContent()
+    }
+
+    private fun findViewsAndInitContent() {
+        //find views
+        tvTitle = findViewById(R.id.tv_title)
+        tvSubTitle = findViewById(R.id.tv_sub_title)
+
+        //init content
+        tvTitle.text = getString(R.string.quiz)
+        tvSubTitle.text = getString(R.string.whatWantToDo)
     }
 }
