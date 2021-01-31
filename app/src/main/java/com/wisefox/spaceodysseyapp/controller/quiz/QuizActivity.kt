9e32cdf9a1be1239455f7022c3415052f44b3dd8
Maxime Traverse <maxime.traverse@orange.fr>
2021-01-31 18:20:37 +0100

    create quizActivity base

---
+++ app/src/main/java/com/wisefox/spaceodysseyapp/controller/quiz/QuizActivity.kt
@@ -0,0 +1,12 @@
+package com.wisefox.spaceodysseyapp.controller.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wisefox.spaceodysseyapp.R
+
+class QuizActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+    }
+}
