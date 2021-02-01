690f40bc57d33a34cb36f6fb1de5852ff69ec6ff
Maxime Traverse <maxime.traverse@orange.fr>
2021-02-01 20:54:52 +0100

    create play quiz activity & its layout

---
+++ app/src/main/java/com/wisefox/spaceodysseyapp/controller/quiz/PlayQuizActivity.kt
@@ -0,0 +1,12 @@
+package com.wisefox.spaceodysseyapp.controller.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wisefox.spaceodysseyapp.R
+
+class PlayQuizActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_quiz)
+    }
+}
