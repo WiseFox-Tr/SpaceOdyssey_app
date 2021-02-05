4d72ab454f7db7dd4d86b63790818df14606a1e4
Maxime Traverse <maxime.traverse@orange.fr>
2021-02-05 14:28:11 +0100

    add getQuestions WS utils method, it send params to server (theme & level) and retrieve in nominal case a list of questions

---
+++ app/src/main/java/com/wisefox/spaceodysseyapp/model/webServices/WSUtils.kt
@@ -0,0 +1,38 @@
+package com.wisefox.spaceodysseyapp.model.webServices
+
+import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.wisefox.spaceodysseyapp.model.ParamsBean
+import com.wisefox.spaceodysseyapp.model.QuestionBean
+import com.wisefox.spaceodysseyapp.model.ResponseCodeBean
+import com.wisefox.spaceodysseyapp.model.webServices.OkHttp.Companion.sendPostOkHttpRequest
+import com.wisefox.spaceodysseyapp.utils.Const
+
+class WSUtils {
+
+    companion object {
+        private val gson = Gson()
+        private var jsonToSend = ""
+
+        //method getQuestions --> send params to server (theme & level) and retrieve a list of questions
+        fun getQuestions(paramsBean: ParamsBean): List<QuestionBean> {
+
+            jsonToSend = gson.toJson(paramsBean)
+            val jsonReceived = sendPostOkHttpRequest(Const.URL_API_GET_QUESTIONS, jsonToSend)
+
+            val itemType = object : TypeToken<ResponseCodeBean<List<QuestionBean>>>() {}.type
+            val responseCodeBean = gson.fromJson<ResponseCodeBean<List<QuestionBean>>>(jsonReceived, itemType)
+
+            Log.d(Const.TAG_WS, "method getQuestions : --> responseCode = $responseCodeBean")
+
+            val questionsList = responseCodeBean.data
+
+            when {
+                responseCodeBean.code != 200 -> throw Exception(responseCodeBean.message ?: "An error occurred...")
+                questionsList.isNullOrEmpty() -> throw Exception("Empty or nonexistent list")
+                else -> return questionsList
+            }
+        }
+    }
+}
