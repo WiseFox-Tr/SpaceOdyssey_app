a4c788d382e65a9472ab5e4a15d95c5aa457eec1
Maxime Traverse <maxime.traverse@orange.fr>
2021-02-05 14:29:03 +0100

    call in an other thread getQuestions from WS utils & if it works well, start playQuizActivity

--- app/src/main/java/com/wisefox/spaceodysseyapp/controller/quiz/SetUpQuizActivity.kt
+++ app/src/main/java/com/wisefox/spaceodysseyapp/controller/quiz/SetUpQuizActivity.kt
@@ -3,15 +3,31 @@ package com.wisefox.spaceodysseyapp.controller.quiz
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import com.wisefox.spaceodysseyapp.R
+import com.wisefox.spaceodysseyapp.model.LevelBean
+import com.wisefox.spaceodysseyapp.model.ParamsBean
+import com.wisefox.spaceodysseyapp.model.ThemeBean
+import com.wisefox.spaceodysseyapp.model.webServices.WSUtils
+import com.wisefox.spaceodysseyapp.utils.Const
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+import kotlin.Exception
 
 class SetUpQuizActivity : AppCompatActivity() {
 
     // Graphics
     private lateinit var tvTitle: TextView
     private lateinit var tvSubTitle: TextView
+
+    //data
+    private var params = ParamsBean(
+        levelBean = LevelBean(1, "Débutant"),
+        themeBean = ThemeBean(1, "Systèmes planétaires")
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +46,20 @@ class SetUpQuizActivity : AppCompatActivity() {
     }
 
     fun onClickPlayQuiz(view: View) {
-        val intentPlayQuizActivity = Intent(this, PlayQuizActivity::class.java)
-        startActivity(intentPlayQuizActivity)
+
+        CoroutineScope(IO).launch {
+            //request for questions & intent PlayQuizActivity
+            try {
+                val questionList = WSUtils.getQuestions(params)
+                Log.d(Const.TAG_CONTROLLER, "List of questions retrieved : --> $questionList")
+                //after intent new activity
+                val intentPlayQuizActivity = Intent(this@SetUpQuizActivity, PlayQuizActivity::class.java)
+                startActivity(intentPlayQuizActivity)
+            }
+            catch (e :Exception) {
+                e.printStackTrace()
+                //todo: update UI by displaying an error message
+            }
+        }
     }
 }
