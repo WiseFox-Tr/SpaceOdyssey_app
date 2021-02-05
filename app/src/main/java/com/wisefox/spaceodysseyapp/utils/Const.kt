12b8c65dea9703fcf3f660c58c1e6e00b655e4ae
Maxime Traverse <maxime.traverse@orange.fr>
2021-02-05 14:20:00 +0100

    add const for tags & web services url

---
+++ app/src/main/java/com/wisefox/spaceodysseyapp/utils/Const.kt
@@ -0,0 +1,13 @@
+package com.wisefox.spaceodysseyapp.utils
+
+class Const {
+    companion object {
+        //TAG Logs
+        const val TAG_WS = "WSUtils"
+        const val TAG_CONTROLLER = "Controller"
+
+        //URL
+        private const val URL_ROOT_API = "http://10.0.2.2:8080"
+        const val URL_API_GET_QUESTIONS = "$URL_ROOT_API/getQuestions"
+    }
+}
