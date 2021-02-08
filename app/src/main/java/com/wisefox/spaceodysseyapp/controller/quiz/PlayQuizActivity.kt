c4d2f35737c34c3642ea15736f3da04f7de89eba
Maxime Traverse <maxime.traverse@orange.fr>
2021-02-08 14:41:24 +0100

    recover quiz object from setUpQuizActivity to use it here

--- app/src/main/java/com/wisefox/spaceodysseyapp/controller/quiz/PlayQuizActivity.kt
+++ app/src/main/java/com/wisefox/spaceodysseyapp/controller/quiz/PlayQuizActivity.kt
@@ -1,9 +1,12 @@
 package com.wisefox.spaceodysseyapp.controller.quiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.wisefox.spaceodysseyapp.R
+import com.wisefox.spaceodysseyapp.model.Quiz
+import com.wisefox.spaceodysseyapp.utils.Const
 
 class PlayQuizActivity : AppCompatActivity() {
 
@@ -11,10 +14,20 @@ class PlayQuizActivity : AppCompatActivity() {
     private lateinit var tvTitle: TextView
     private lateinit var tvSubTitle: TextView
 
+    //Data
+    private lateinit var quiz : Quiz
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_quiz)
         findViewsAndInitContent()
+
+        quiz = intent.getSerializableExtra("quiz") as Quiz
+
+        Log.d(Const.TAG_CONTROLLER, "LIST OF QUESTIONS RETRIEVED BY PLAY A QUIZ ACTIVITY : \n\n")
+        for(i in quiz.questions.indices) {
+            Log.d(Const.TAG_CONTROLLER, "question ${i}: --> ${quiz.questions[i]}")
+        }
     }
 
     private fun findViewsAndInitContent() {
