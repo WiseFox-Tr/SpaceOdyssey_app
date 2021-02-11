f4997c54d1d6aae9d127448c8ce406ef4198aac2
Maxime Traverse <maxime.traverse@orange.fr>
2021-02-11 17:06:23 +0100

    add a class for custom graphics components

---
+++ app/src/main/java/com/wisefox/spaceodysseyapp/view/CustomizeGraphicsComponent.kt
@@ -0,0 +1,34 @@
+package com.wisefox.spaceodysseyapp.view
+
+import android.graphics.Typeface
+import android.view.View
+import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
+import com.wisefox.spaceodysseyapp.R
+
+class CustomizeGraphicsComponent {
+    companion object {
+        fun setSnackbarStyle(
+                snackbar: Snackbar,
+                backgroundTintColor: Int = snackbar.context.getColor(R.color.text_red),
+                textColor: Int = snackbar.context.getColor(R.color.white),
+                textSize: Float = 14f,
+                minHeight : Int = 300,
+                textAlignment: Int = TextView.TEXT_ALIGNMENT_CENTER
+        ) {
+            //set colors properties
+            snackbar.setTextColor(textColor)
+            snackbar.setBackgroundTint(backgroundTintColor)
+
+            //set text properties
+            val snackbarText = snackbar.view.findViewById<View>(com.google.android.material.R.id.snackbar_text) as TextView
+            snackbarText.textSize = textSize
+            snackbarText.textAlignment = textAlignment
+            snackbarText.setTypeface(snackbarText.typeface, Typeface.BOLD)
+
+            //set height properties
+            val snackbarView = snackbar.view
+            snackbarView.minimumHeight = minHeight
+        }
+    }
+}
