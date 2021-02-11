f2dc751fa48c3d6c420e7ba7a4ca2d00cf761b80
Maxime Traverse <maxime.traverse@orange.fr>
2021-02-11 11:35:22 +0100

    add param attribute

--- app/src/main/java/com/wisefox/spaceodysseyapp/model/QuizBean.kt
+++ app/src/main/java/com/wisefox/spaceodysseyapp/model/QuizBean.kt
@@ -4,6 +4,7 @@ import java.io.Serializable
 
 class QuizBean (
         val questions: List<QuestionBean>,
+        val params: ParamsBean,
         val nbQuestions: Int = 10,
         var nbGoodAnswer: Int = 0,
         var score: Int = 0,
