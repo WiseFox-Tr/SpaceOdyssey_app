24845f97a7bae27fd4650df8934c2954a6ba1fa6
Maxime Traverse <maxime.traverse@orange.fr>
2021-03-01 21:21:01 +0100

    add some methods - implementNbGoodQuestions, updateScore & override toString

--- app/src/main/java/com/wisefox/spaceodysseyapp/model/Quiz.kt
+++ app/src/main/java/com/wisefox/spaceodysseyapp/model/Quiz.kt
@@ -5,8 +5,27 @@ import java.io.Serializable
 class Quiz (
         val questions: List<Question>,
         val params: Params,
-        val nbQuestions: Int = 10,
+        val nbQuestions: Int = questions.size,
         var nbGoodAnswer: Int = 0,
         var score: Int = 0,
         val time : Double = 10.0
-        ) : Serializable
+        ) : Serializable {
+                fun incrementNbGoodAnswers() {
+                        this.nbGoodAnswer++
+                }
+
+                fun updateScore(score: Int) {
+                        this.score += score
+                }
+
+                override fun toString(): String {
+                        val description = "This quiz contain ${this.nbQuestions} questions.It's response duration is ${this.time} sec.\n" +
+                                "The current score is ${this.score} for ${this.nbGoodAnswer} good answers.\n"
+                        var questionDescription = "This questions are : \n\n"
+
+                        for((index, question) in this.questions.withIndex()) {
+                                questionDescription += "question n°${index} : $question... \n"
+                        }
+                        return  description + questionDescription
+                }
+        }
