00db87f2625881a52adb0f4bac01f141033bc51a
Maxime Traverse <maxime.traverse@orange.fr>
2021-03-03 16:32:45 +0100

    implement the try to retrieve params from server & a catch bloc

--- app/src/main/java/com/wisefox/spaceodysseyapp/controller/quiz/QuizActivity.kt
+++ app/src/main/java/com/wisefox/spaceodysseyapp/controller/quiz/QuizActivity.kt
@@ -1,17 +1,24 @@
 package com.wisefox.spaceodysseyapp.controller.quiz
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.wisefox.spaceodysseyapp.R
+import com.wisefox.spaceodysseyapp.controller.CommonController
+import com.wisefox.spaceodysseyapp.model.webServices.WebServices
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
 
 class QuizActivity : AppCompatActivity() {
 
     // Graphics
     private lateinit var tvTitle: TextView
     private lateinit var tvSubTitle: TextView
+    private lateinit var rootView: View
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +31,7 @@ class QuizActivity : AppCompatActivity() {
         //find views
         tvTitle = findViewById(R.id.tv_title)
         tvSubTitle = findViewById(R.id.tv_subTitle)
+        rootView = findViewById(R.id.root_quiz)
 
         //init content
         tvTitle.text = getString(R.string.quiz)
@@ -31,8 +39,27 @@ class QuizActivity : AppCompatActivity() {
     }
 
     fun onClickSetUpQuiz(view: View) {
-        val intentSetUpQuizActivity = Intent(this, SetUpQuizActivity::class.java)
-        startActivity(intentSetUpQuizActivity)
-        finish()
+        //todo: add progress bar
+        CoroutineScope(IO).launch {
+            try {
+                //request for params
+                val paramsRetrieved = WebServices.getParams()
+
+                val intentSetUpQuizActivity = Intent(this@QuizActivity, SetUpQuizActivity::class.java)
+                intentSetUpQuizActivity.putExtra("params", paramsRetrieved)
+                startActivity(intentSetUpQuizActivity)
+                finish()
+            }
+            catch (e: Exception) {
+                e.printStackTrace()
+                launch(Main) {
+                    CommonController.displaySnackbar(
+                            rootView = rootView,
+                            message = CommonController.manageError(exception = e, context = this@QuizActivity),
+                            context = this@QuizActivity
+                    )
+                }
+            }
+        }
     }
 }
