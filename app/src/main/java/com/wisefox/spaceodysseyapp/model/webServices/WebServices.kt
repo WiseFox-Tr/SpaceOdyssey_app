33a2412deef6135a67953c832aedc8639b6bbe07
Maxime Traverse <maxime.traverse@orange.fr>
2021-03-03 16:32:11 +0100

    implement getParams method

--- app/src/main/java/com/wisefox/spaceodysseyapp/model/webServices/WebServices.kt
+++ app/src/main/java/com/wisefox/spaceodysseyapp/model/webServices/WebServices.kt
@@ -15,6 +15,21 @@ class WebServices {
         private var jsonToSend = ""
         private var jsonReceived = ""
 
+        fun getParams(): Params {
+            jsonReceived = OkHttp.sendGetOkHttpRequest(AppConst.URL_API_GET_PARAMS)
+
+            val itemType = object : TypeToken<ServerResponse<Params>>() {}.type
+            val responseCodeBean = gson.fromJson<ServerResponse<Params>>(jsonReceived, itemType)
+
+            Log.v(AppConst.TAG_WS, "method getParams : --> responseCode = $responseCodeBean")
+            when {
+                responseCodeBean.code != 200 -> throw Exception("error ${responseCodeBean.code} : ${responseCodeBean.message}")
+                responseCodeBean.data == null -> throw Exception("No content in data")
+                else -> return responseCodeBean.data //List<QuestionBean>
+            }
+
+        }
+
         //method getQuestions --> send params to server (theme & level) and retrieve a list of questions
         fun getQuestions(params: Params): List<Question> {
 
@@ -24,7 +39,7 @@ class WebServices {
             val itemType = object : TypeToken<ServerResponse<List<Question>>>() {}.type
             val responseCodeBean = gson.fromJson<ServerResponse<List<Question>>>(jsonReceived, itemType)
 
-            Log.d(AppConst.TAG_WS, "method getQuestions : --> responseCode = $responseCodeBean")
+            Log.v(AppConst.TAG_WS, "method getQuestions : --> responseCode = $responseCodeBean")
             when {
                 responseCodeBean.code != 200 -> throw Exception("error ${responseCodeBean.code} : ${responseCodeBean.message}")
                 responseCodeBean.data.isNullOrEmpty() -> throw Exception("No content in data")
