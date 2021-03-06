f48baa2a7936145dba5bd91304f1e66b3e5eaad3
Maxime Traverse <maxime.Traverse@orange.fr>
2021-03-06 20:26:51 +0100

    add callback for onItemSelected method

--- app/src/main/java/com/wisefox/spaceodysseyapp/controller/CommonController.kt
+++ app/src/main/java/com/wisefox/spaceodysseyapp/controller/CommonController.kt
@@ -1,9 +1,14 @@
 package com.wisefox.spaceodysseyapp.controller
 
 import android.content.Context
+import android.content.Intent
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import com.google.android.material.snackbar.Snackbar
 import com.wisefox.spaceodysseyapp.R
+import com.wisefox.spaceodysseyapp.controller.quiz.QuizActivity
 import com.wisefox.spaceodysseyapp.view.CustomGraphicComponents
 
 
@@ -21,6 +26,23 @@ class CommonController {
             val snackbar = Snackbar.make(rootView, message, Snackbar.LENGTH_LONG)
             CustomGraphicComponents.setSnackbarStyle(snackbar = snackbar)
             snackbar.show()
+        }
+
+        fun itemSelectedCallback(item: MenuItem, context: Context) {
+            when(item.itemId) {
+                R.id.menu_account -> Toast.makeText(context, "Account clicked", Toast.LENGTH_LONG).show()
+                R.id.menu_mainMenu ->  {
+                    val intentMainActivity = Intent(context, MainActivity::class.java)
+                    startActivity(context, intentMainActivity, null)
+                }
+                R.id.menu_lesson ->  Toast.makeText(context, "Lesson clicked", Toast.LENGTH_LONG).show()
+                R.id.menu_quiz ->  {
+                    val intentQuizActivity = Intent(context, QuizActivity::class.java)
+                    startActivity(context, intentQuizActivity, null)
+                }
+                R.id.menu_about ->  Toast.makeText(context, "About clicked", Toast.LENGTH_LONG).show()
+            }
+            //todo: add for each activity a finish() 
         }
     }
 }
