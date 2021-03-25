f919ec963fe17e2cff950b0750fe91a2caa22827
Maxime Traverse <maxime.Traverse@orange.fr>
2021-03-25 19:48:19 +0100

    add app bar methods & variables

--- app/src/main/java/com/wisefox/spaceodysseyapp/controller/account/LogInActivity.kt
+++ app/src/main/java/com/wisefox/spaceodysseyapp/controller/account/LogInActivity.kt
@@ -2,16 +2,20 @@ package com.wisefox.spaceodysseyapp.controller.account
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.wisefox.spaceodysseyapp.R
+import com.wisefox.spaceodysseyapp.controller.CommonController
 import com.wisefox.spaceodysseyapp.view.CustomGraphicComponents
 
 class LogInActivity : AppCompatActivity(), View.OnClickListener {
 
+    private lateinit var appbar: androidx.appcompat.widget.Toolbar
     private lateinit var tiLogInPseudo: TextInputLayout
     private lateinit var etLogInPseudo: TextInputEditText
     private lateinit var tiLogInPassword: TextInputLayout
@@ -23,10 +27,12 @@ class LogInActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
         findViewsAndInitContent()
+        setSupportActionBar(appbar)
     }
 
     private fun findViewsAndInitContent() {
         //find views
+        appbar = findViewById(R.id.appBar)
         tiLogInPseudo = findViewById(R.id.ti_logInPseudo)
         etLogInPseudo = findViewById(R.id.et_logInPseudo)
         tiLogInPassword = findViewById(R.id.ti_logInPassword)
@@ -43,10 +49,23 @@ class LogInActivity : AppCompatActivity(), View.OnClickListener {
         btnGoToRegisterScreen.setOnClickListener(this)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        CommonController.itemSelectedCallback(item, this)
+        finish()
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onClick(v: View?) {
         when(v) {
             btnLogIn -> { Toast.makeText(this, "Try to Log In", Toast.LENGTH_SHORT).show() }
             btnGoToRegisterScreen -> { Toast.makeText(this, "Want to go to register screen !", Toast.LENGTH_SHORT).show() }
         }
     }
+
+
 }
