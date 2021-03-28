e84399bcf5a2a627da1c3e036e65f957d5b7f056
Maxime Traverse <maxime.Traverse@orange.fr>
2021-03-28 21:19:54 +0200

    add const values for full string exception messages

--- app/src/main/java/com/wisefox/spaceodysseyapp/model/AppConst.kt
+++ app/src/main/java/com/wisefox/spaceodysseyapp/model/AppConst.kt
@@ -11,9 +11,21 @@ class AppConst {
         const val URL_API_GET_PARAMS = "$URL_ROOT_API/getParams"
         const val URL_API_GET_QUESTIONS = "$URL_ROOT_API/getQuestions"
 
-        //score multiplier
+        //score multiplier for quiz
         const val scoreMultiplierEasy = 2
         const val scoreMultiplierMedium = 4
         const val scoreMultiplierConfirmed = 7
+
+        //max length for editText
+        const val maxPseudoLength = 25
+        const val maxMailLength = 200
+        const val maxPasswordLength = 30
+
+        //exception text values
+        const val EXCEPTION_EMPTY_FIELD = "Edit text content is empty"
+        const val EXCEPTION_LENGTH_OVERFLOW = "Edit text content length exceed max length"
+        const val EXCEPTION_MAIL_FORMAT = "Mail format is invalid"
+        const val EXCEPTION_PASSWORD_FORMAT = "Password format is invalid"
+        const val EXCEPTION_PASSWORDS_DO_NOT_MATCH = "Passwords don't match"
     }
 }
